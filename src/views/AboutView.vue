58260e1b291567d82b602081d87a1bccea1acccb
tankaio <2367458415@qq.com>
2025-04-21 14:24:55 +0700

    initial

---
+++ src/views/AboutView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div class="imgs">
+      <img src="../assets/logo.svg" alt="" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.imgs {
+  img {
+    width: 120px;
+  }
+}
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
